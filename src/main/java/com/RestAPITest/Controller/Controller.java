6df6f6d262395d8283dd8cf9657b74d28741d9fa
afroz3579 <afroz2016@gmail.com>
2023-12-29 12:04:47 +0530

    first commit

--- src/main/java/com/RestAPITest/Controller/Controller.java
+++ src/main/java/com/RestAPITest/Controller/Controller.java
@@ -3,8 +3,12 @@ package com.RestAPITest.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.RestAPITest.Entity.BookStoreEntity;
@@ -14,17 +18,42 @@ import com.RestAPITest.Service.ServiceBook;
 public class Controller {
 	@Autowired
 	private ServiceBook service;
+	
+	//get all books
 	@GetMapping("/bookstore")
 	public List<BookStoreEntity> getData() {
 		
 	return this.service.getBookStoreData();
 	}
 	
+	//get specific book
 	@GetMapping("/bookstore/{id}")
 	public BookStoreEntity getOneBook(@PathVariable("id")int id) {
 		return this.service.getBookById(id);
 	}
 	
+	//add book
+	@PostMapping("/bookstore")
+	public BookStoreEntity addBooks(@RequestBody BookStoreEntity b) {
+		BookStoreEntity book=this.service.addBook(b);
+		System.out.println(b);
+		return book;
+		
+	}
 	
-
+	//delete book by id
+	@DeleteMapping("/bookstore/{bookId}")
+	public void  deleteBook(@PathVariable("bookId") int bookId) {
+		
+		this.service.deleteBoodById(bookId);
+	}
+	
+	//update book
+	@PutMapping("/bookstore/{bookid}")
+	public void updateBook(@RequestBody BookStoreEntity book ,@PathVariable("bookid") int bookid) {
+		//TODO: process PUT request
+	this.service.updateBook(book,bookid);
+		
+	}
+	
 }
--- src/main/java/com/RestAPITest/Service/ServiceBook.java
+++ src/main/java/com/RestAPITest/Service/ServiceBook.java
@@ -2,6 +2,8 @@ package com.RestAPITest.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,30 @@ public class ServiceBook {
 		return book1;
 	}
 	
+     public BookStoreEntity addBook(BookStoreEntity book2) {
+    	 book.add(book2);
+    	return book2;
+     }
+     
+     
+     public void deleteBoodById(int bookId) {
+    	 
+    	book= book.stream().filter(book->book.getId() != bookId).collect(Collectors.toList());
+     }
 
+	public void updateBook(BookStoreEntity book2 , int bookid) {
+		// TODO Auto-generated method stub
+		
+	book=book.stream().map(book->{
+			if(book.getId()==bookid) {
+				book.setTitleName(book2.getTitleName());
+				book.setAuthorName(book2.getAuthorName());
+		
+			}
+			return book	;
+		}).collect(Collectors.toList());
+		
+	}
+     
+     
 }
